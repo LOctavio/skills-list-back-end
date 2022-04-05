4d81126c35ee6a2dc28296f9c2a7bcf2e151d376
LOctavio <luis_ram1997@hotmail.com>
2022-04-05 17:52:48 -0500

    Add column to schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_05_203810) do
   enable_extension "plpgsql"
 
   create_table "proficiencies", force: :cascade do |t|
+    t.text "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
