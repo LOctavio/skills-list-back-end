0da9e4b97e4e9b2ac06dabb63e9a5c61b75c1790
LOctavio <luis_ram1997@hotmail.com>
2022-04-05 20:22:55 -0500

    Add method to show people with same skill

--- app/models/UserSkill.rb
+++ app/models/UserSkill.rb
@@ -2,4 +2,9 @@ class UserSkill < ApplicationRecord
   belongs_to :user
   belongs_to :skill
   belongs_to :proficiency
+
+  def people_with_same_skill
+    username = user.username
+    skill.users.all.where.not(username: username)
+  end
 end
